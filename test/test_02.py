1386482f78d4e727a4a91da54dfe9d6e0026863f
Master-sum <285957109@qq.com>
2020-11-10 23:50:22 +0800

    pytest学习中....

--- test/test_02.py
+++ test/test_02.py
@@ -1,11 +1,12 @@
 """
 作者   ：bjx
-创建时间   ：2020/11/10  11:30 下午 
-文件名称   ：test.PY
+创建时间   ：2020/11/10  11:38 下午 
+文件名称   ：test_02.PY
 开发工具   ：PyCharm
 """
+
 def func(x):
     return x + 1
 
 def test_answer():
-    assert func(3) == 5
+    assert func(4) == 5
