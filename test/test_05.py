57196d7987454031fcb50cbed34d3b86607d50c6
Master-sum <285957109@qq.com>
2020-11-12 00:10:42 +0800

    pytest学习中....pdb

---
+++ test/test_05.py
@@ -0,0 +1,13 @@
+"""
+作者   ：bjx
+创建时间   ：2020/11/11  11:12 上午 
+文件名称   ：test_05.PY
+开发工具   ：PyCharm
+坚持：山穷水复疑无路，柳暗花明又一村
+"""
+import pytest
+
+
+@pytest.mark.test_id(1501)
+def test_function():
+    assert True
